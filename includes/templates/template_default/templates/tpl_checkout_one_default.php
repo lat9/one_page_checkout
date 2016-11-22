fb13d5719d2a3853c5390dd4abc93aa13baa9cb6
lat9 <lat9@vinosdefrutastropicales.com>
2016-11-22 09:23:02 -0500

    #59:  Correct virtual-order handling
    Correct the display of the checkout_one page's submit button when the
    order contains only virtual products or qualifies for free shipping.

--- includes/templates/template_default/templates/tpl_checkout_one_default.php
+++ includes/templates/template_default/templates/tpl_checkout_one_default.php
@@ -10,7 +10,7 @@ echo $payment_modules->javascript_validation();
 // -----
 // Gather the count of enabled shipping- and payment-methods, so that only applicable sections are displayed.
 //
-$shipping_module_available = (zen_count_shipping_modules () > 0);
+$shipping_module_available = ($free_shipping || $is_virtual_order || zen_count_shipping_modules () > 0);
 $enabled_payment_modules = $payment_modules->selection();
 $payment_module_available = ($payment_modules->in_special_checkout() || count ($enabled_payment_modules > 0));
 ?>
