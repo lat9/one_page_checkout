b9bf4c68999e20fefaffe28ff56609d560ff7a29
lat9 <lat9@vinosdefrutastropicales.com>
2019-03-11 11:14:38 -0400

    #176: Correct PHP Notice for untaxed shipping
    ... includes some refactoring for readability.

--- includes/templates/template_default/templates/tpl_modules_checkout_one_shipping.php
+++ includes/templates/template_default/templates/tpl_modules_checkout_one_shipping.php
@@ -1,18 +1,19 @@
 <?php
 // -----
-// Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9 (cindy@vinosdefrutastropicales.com).
-// Copyright (C) 2013-2016, Vinos de Frutas Tropicales.  All rights reserved.
+// Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9
+// Copyright (C) 2013-2019, Vinos de Frutas Tropicales.  All rights reserved.
 //
 // -----
 // This module is used by both the plugin's tpl_modules_opc_shipping_choices.php template and by the plugin's AJAX handler (zcAjaxOnePageCheckout.php) to render
 // the contents of the order's shipping section.
 //
             $radio_buttons = 0;
-            for ($i = 0, $n = count ($quotes); $i < $n; $i++) {
+            for ($i = 0, $n = count($quotes); $i < $n; $i++) {
+                $shipping_module_id = $quotes[$i]['id'];
                 if ($quotes[$i]['module'] != '') { // Standard
 ?>
         <fieldset>
-          <legend id="<?php echo $quotes[$i]['id']; ?>"><?php echo $quotes[$i]['module']; ?>&nbsp;<?php if (isset($quotes[$i]['icon']) && zen_not_null($quotes[$i]['icon'])) { echo $quotes[$i]['icon']; } ?></legend>
+          <legend id="<?php echo $quotes[$i]['id']; ?>"><?php echo $quotes[$i]['module']; ?>&nbsp;<?php echo (!empty($quotes[$i]['icon'])) ? $quotes[$i]['icon'] : ''; ?></legend>
 
 <?php
                     if (isset($quotes[$i]['error'])) {
@@ -20,32 +21,43 @@
           <div><?php echo $quotes[$i]['error']; ?></div>
 <?php
                     } else {
-                        for ($j = 0, $n2 = count ($quotes[$i]['methods']); $j < $n2; $j++) {
+                        $shipping_tax_rate = (empty($quotes[$i]['tax'])) ? 0 : $quotes[$i]['tax'];
+                        for ($j = 0, $n2 = count($quotes[$i]['methods']); $j < $n2; $j++) {
+                            $shipping_method_id = $quotes[$i]['methods'][$j]['id'];
 // set the radio button to be checked if it is the method chosen
-                            $shipping_method = $quotes[$i]['id'] . '_' . $quotes[$i]['methods'][$j]['id'];
+                            $shipping_method = $shipping_module_id . '_' . $shipping_method_id;
                             $checked = ($shipping_method == $_SESSION['shipping']['id']);
+                            
+                            $shipping_method_cost = $quotes[$i]['methods'][$j]['cost'];
 
                             if ($n > 1 || $n2 > 1) {
 ?>
-          <div class="important forward" id="<?php echo str_replace (' ', '-', $shipping_method) . '-value'; ?>"><?php echo $currencies->format (zen_add_tax ($quotes[$i]['methods'][$j]['cost'], (isset ($quotes[$i]['tax']) ? $quotes[$i]['tax'] : 0))); ?></div>
+          <div class="important forward" id="<?php echo str_replace(' ', '-', $shipping_method) . '-value'; ?>"><?php echo $currencies->format(zen_add_tax($shipping_method_cost, $shipping_tax_rate)); ?></div>
 <?php
                             } else {
 ?>
-          <div class="important forward"><?php echo $currencies->format (zen_add_tax ($quotes[$i]['methods'][$j]['cost'], $quotes[$i]['tax'])) . zen_draw_hidden_field ('shipping', $shipping_method); ?></div>
+          <div class="important forward"><?php echo $currencies->format(zen_add_tax($shipping_method_cost, $shipping_tax_rate)) . zen_draw_hidden_field('shipping', $shipping_method); ?></div>
 <?php
                             }
-                             echo '<div class="custom-control custom-radio">';        
-                            echo zen_draw_radio_field('shipping', $shipping_method, $checked, 'id="ship-'.$quotes[$i]['id'] . '-' . str_replace(' ', '-', $quotes[$i]['methods'][$j]['id']) .'"'); 
 ?>
-          <label for="ship-<?php echo $quotes[$i]['id'] . '-' . str_replace(' ', '-', $quotes[$i]['methods'][$j]['id']); ?>" class="custom-control-label radioButtonLabel" id="<?php echo str_replace (' ', '-', $shipping_method); ?>-title"><?php echo $quotes[$i]['methods'][$j]['title']; ?></label>
-                    </div>
-                    <br class="clearBoth" />
+          <div class="custom-control custom-radio">
+<?php
+                            $shipping_module_method_id = 'ship-' . $shipping_module_id . '-' . str_replace(' ', '-', $shipping_method_id);
+                            $shipping_method_title = $quotes[$i]['methods'][$j]['title'];
+                            $shipping_method_title_id = str_replace(' ', '-', $shipping_method) . '-title';
+                            echo zen_draw_radio_field('shipping', $shipping_method, $checked, 'id="' . $shipping_module_method_id . '"'); 
+?>
+            <label for="<?php echo $shipping_module_method_id; ?>" class="custom-control-label radioButtonLabel" id="<?php echo $shipping_method_title_id; ?>"><?php echo $shipping_method_title; ?></label>
+          </div>
+          <br class="clearBoth" />
 <?php
                             $radio_buttons++;
                         }
 
-                        if (isset ($quotes[$i]['html_input'])) {
-                            echo '<div class="shipping-additional-inputs">' . $quotes[$i]['html_input'] . '</div>';
+                        if (isset($quotes[$i]['html_input'])) {
+?>
+          <div class="shipping-additional-inputs"><?php echo $quotes[$i]['html_input']; ?></div>
+<?php
                         }
                     }
 ?>
