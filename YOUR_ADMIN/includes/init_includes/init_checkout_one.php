8a62d637fa107baa624523c5ce0b43fc445a98d7
lat9 <lat9@vinosdefrutastropicales.com>
2019-03-27 13:25:26 -0400

    Update version to 2.1.0-beta7

--- YOUR_ADMIN/includes/init_includes/init_checkout_one.php
+++ YOUR_ADMIN/includes/init_includes/init_checkout_one.php
@@ -15,8 +15,8 @@ if (!defined('IS_ADMIN_FLAG')) {
 // 500-599 ... Registered-account settings
 // 1000+ ..... Debug settings
 //
-define('CHECKOUT_ONE_CURRENT_VERSION', '2.1.0-beta6');
-define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2019-03-17');
+define('CHECKOUT_ONE_CURRENT_VERSION', '2.1.0-beta7');
+define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2019-03-27');
 
 if (isset($_SESSION['admin_id'])) {
     $version_release_date = CHECKOUT_ONE_CURRENT_VERSION . ' (' . CHECKOUT_ONE_CURRENT_UPDATE_DATE . ')';
