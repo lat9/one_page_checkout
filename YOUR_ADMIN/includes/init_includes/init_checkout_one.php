141fea4f30c6da147e4d31710deaab6baf7a3c3e
lat9 <lat9@vinosdefrutastropicales.com>
2019-07-07 08:29:36 -0400

    #204: Update admin version, too.

--- YOUR_ADMIN/includes/init_includes/init_checkout_one.php
+++ YOUR_ADMIN/includes/init_includes/init_checkout_one.php
@@ -15,8 +15,8 @@ if (!defined('IS_ADMIN_FLAG')) {
 // 500-599 ... Registered-account settings
 // 1000+ ..... Debug settings
 //
-define('CHECKOUT_ONE_CURRENT_VERSION', '2.1.2');
-define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2019-07-02');
+define('CHECKOUT_ONE_CURRENT_VERSION', '2.1.3-beta1');
+define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2019-07-07');
 
 if (isset($_SESSION['admin_id'])) {
     $version_release_date = CHECKOUT_ONE_CURRENT_VERSION . ' (' . CHECKOUT_ONE_CURRENT_UPDATE_DATE . ')';
