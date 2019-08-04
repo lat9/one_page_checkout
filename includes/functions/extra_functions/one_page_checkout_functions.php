35d0dea8e448e1ccfb35cbfd1edf4f73c4113232
lat9 <lat9@vinosdefrutastropicales.com>
2019-08-04 09:49:43 -0400

    #207: Clean up guest address-book ...
    ... removing unwanted entries that might have been previously recorded.

--- includes/functions/extra_functions/one_page_checkout_functions.php
+++ includes/functions/extra_functions/one_page_checkout_functions.php
@@ -1,8 +1,51 @@
 <?php
 // -----
 // Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9 (cindy@vinosdefrutastropicales.com).
-// Copyright (C) 2013-2018, Vinos de Frutas Tropicales.  All rights reserved.
+// Copyright (C) 2013-2019, Vinos de Frutas Tropicales.  All rights reserved.
 //
+
+// -----
+// For versions of OPC prior to v2.1.0, it was possible that additional address-book entries were recorded
+// for the temporary (guest) account.  We'll clean those up, if present, on each page-load, recording any
+// addresses found for the store-owner's inspection.
+//
+if (defined('CHECKOUT_ONE_ENABLED') && defined('CHECKOUT_ONE_GUEST_CUSTOMER_ID')) {
+    $check = $db->Execute(
+        "SELECT COUNT(*) AS count
+           FROM " . TABLE_ADDRESS_BOOK . "
+          WHERE customers_id = " . (int)CHECKOUT_ONE_GUEST_CUSTOMER_ID,
+          false,
+          false,
+          0,
+          true
+    );
+    if ($check->fields['count'] > 2) {
+        $entry_count = $check->fields['count'] - 2;
+        $entries = $db->Execute(
+            "SELECT *
+               FROM " . TABLE_ADDRESS_BOOK . "
+              WHERE customers_id = " . (int)CHECKOUT_ONE_GUEST_CUSTOMER_ID . "
+              ORDER BY address_book_id DESC
+              LIMIT $entry_count",
+              false,
+              false,
+              0,
+              true
+        );
+        $entries_to_remove = array();
+        $log_file_name = DIR_FS_LOGS . '/opc_address_book_cleanup.log';
+        error_log(date('Y-m-d H:i:s') . ": Removing $entry_count guest address-book entries." . PHP_EOL, 3, $log_file_name);
+        while (!$entries->EOF) {
+            error_log(str_replace(',"', ', "', json_encode($entries->fields)) . PHP_EOL, 3, $log_file_name);
+            $entries_to_remove[] = $entries->fields['address_book_id'];
+            $entries->MoveNext();
+        }
+        $db->Execute(
+            "DELETE FROM " . TABLE_ADDRESS_BOOK . "
+              WHERE address_book_id IN (" . implode(', ', $entries_to_remove) . ")"
+        );
+    }
+}
 
 // -----
 // This function identifies whether (true) or not (false) the current customer session is
