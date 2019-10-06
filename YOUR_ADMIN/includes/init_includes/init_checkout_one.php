e3d11ad2f94077cf8405009407412b00e74e506f
lat9 <lat9@vinosdefrutastropicales.com>
2019-10-06 13:56:11 -0400

    #218: Remove 'myDEBUG-' prefix from OPC log names

--- YOUR_ADMIN/includes/init_includes/init_checkout_one.php
+++ YOUR_ADMIN/includes/init_includes/init_checkout_one.php
@@ -41,7 +41,7 @@ if (isset($_SESSION['admin_id'])) {
         
         define('CHECKOUT_ONE_ENABLED', 'false');
         
-        $db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, date_added, sort_order, use_function, set_function ) VALUES ( 'Enable One-Page Checkout Debug?', 'CHECKOUT_ONE_DEBUG', 'false', 'When enabled, debug files named myDEBUG-one_page_checkout-<em>xx</em>.log are created in your /logs folder (<em>xx</em> is the customer_id for the checkout).  Use the <b>true</b> setting in combination with the <em>Debug: Customer List</em> setting to limit the customers for which the debug-action is taken.<br /><br />Default: <b>false</b>', $cgi, now(), 50, NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),')");
+        $db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, date_added, sort_order, use_function, set_function ) VALUES ( 'Enable One-Page Checkout Debug?', 'CHECKOUT_ONE_DEBUG', 'false', 'When enabled, debug files named <code>one_page_checkout-<em>xx</em>.log</code> are created in your /logs folder (<em>xx</em> is the customer_id for the checkout).  Use the <b>true</b> setting in combination with the <em>Debug: Customer List</em> setting to limit the customers for which the debug-action is taken.<br /><br />Default: <b>false</b>', $cgi, now(), 50, NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),')");
         
         $db->Execute("INSERT INTO " . TABLE_CONFIGURATION . " ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, date_added, sort_order, use_function, set_function ) VALUES ( 'Debug: Customer List', 'CHECKOUT_ONE_DEBUG_EXTRA', '', 'When you enable the plugin\'s debug, use this setting to limit the customers for which the debug-logs are generated.  Leave the setting blank (the default) to debug <b>all</b> customers or identify a comma-separated list of customer_id values to limit the debug to just those customers.<br />', $cgi, now(), 51, NULL, NULL)");
     } elseif (!defined('CHECKOUT_ONE_MODULE_VERSION')) {
@@ -64,7 +64,7 @@ if (isset($_SESSION['admin_id'])) {
     if (defined('CHECKOUT_ONE_DEBUG') && strpos(CHECKOUT_ONE_DEBUG, '<b>full</b>') === false) {
         $db->Execute(
             "UPDATE " . TABLE_CONFIGURATION . " 
-                SET configuration_description = 'When enabled, debug files named myDEBUG-one_page_checkout-<em>xx</em>.log are created in your /logs folder (<em>xx</em> is the customer_id for the checkout).  Use the <b>true</b> or <b>full</b> settings in combination with the <em>Debug: Customer List</em> setting to limit the customers for which the debug-action is taken.  Setting the value to <b>full</b> will also set the PHP error-level for the checkout so that <b>all</b> PHP errors are logged.<br /><br />Default: <b>false</b>',
+                SET configuration_description = 'When enabled, debug files named <code>one_page_checkout-<em>xx</em>.log</code> are created in your /logs folder (<em>xx</em> is the customer_id for the checkout).  Use the <b>true</b> or <b>full</b> settings in combination with the <em>Debug: Customer List</em> setting to limit the customers for which the debug-action is taken.  Setting the value to <b>full</b> will also set the PHP error-level for the checkout so that <b>all</b> PHP errors are logged.<br /><br />Default: <b>false</b>',
                     set_function = 'zen_cfg_select_option(array(\'true\', \'false\', \'full\'),'
               WHERE configuration_key = 'CHECKOUT_ONE_DEBUG' LIMIT 1"
         );
@@ -246,6 +246,20 @@ if (isset($_SESSION['admin_id'])) {
         );
     }
     
+    // -----
+    // v2.2.0:
+    //
+    // - Remove 'myDEBUG-' prefix from OPC's debug file names.
+    //
+    if (version_compare(CHECKOUT_ONE_MODULE_VERSION, '2.2.0', '<')) {
+        $db->Execute(
+            "UPDATE " . TABLE_CONFIGURATION . "
+                SET configuration_description = 'When enabled, debug files named <code>one_page_checkout-<em>xx</em>.log</code> are created in your /logs folder (<em>xx</em> is the customer_id for the checkout).  Use the <b>true</b> setting in combination with the <em>Debug: Customer List</em> setting to limit the customers for which the debug-action is taken.<br /><br />Default: <b>false</b>'
+              WHERE configuration_key = 'CHECKOUT_ONE_DEBUG'
+              LIMIT 1"
+        );
+    }
+
     if (CHECKOUT_ONE_MODULE_VERSION != '0.0.0' && CHECKOUT_ONE_MODULE_VERSION != $version_release_date) {
         $messageStack->add(sprintf(TEXT_OPC_UPDATED, CHECKOUT_ONE_MODULE_VERSION, $version_release_date), 'success');
         $db->Execute("UPDATE " . TABLE_CONFIGURATION . " SET configuration_value = '$version_release_date', last_modified = now() WHERE configuration_key = 'CHECKOUT_ONE_MODULE_VERSION' LIMIT 1");
