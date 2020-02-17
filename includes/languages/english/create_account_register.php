0297055de6075078b1e5dcdf74b88f8b8c047357
lat9 <cindy@vinosdefrutastropicales.com>
2020-02-17 12:21:01 -0500

    #235: Capture updated language file, too.

--- includes/languages/english/create_account_register.php
+++ includes/languages/english/create_account_register.php
@@ -1,13 +1,15 @@
 <?php
 // -----
-// Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9 (cindy@vinosdefrutastropicales.com).
-// Copyright (C) 2018, Vinos de Frutas Tropicales.  All rights reserved.
+// Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9
+// Copyright (C) 2018-2020, Vinos de Frutas Tropicales.  All rights reserved.
 //
 define('NAVBAR_TITLE_REGISTER', 'Account Registration');
 define('TEXT_INSTRUCTIONS', '<strong class="note">Note:</strong> If you already have an account with us, please login at our <a href="%s">login</a> page.  We\'ll ask for your address details once you are ready to place an order.');
 define('ENTRY_EMAIL_ADDRESS_CONFIRM', 'Confirm Email:');
 define('ENTRY_EMAIL_FORMAT', 'Email Format:');
 define('BUTTON_SUBMIT_REGISTER_ALT', 'Register');
+
+define('HEADING_CONTACT_DETAILS', 'Contact Details');
 
 define('ENTRY_EMAIL_MISMATCH_ERROR', 'The <em>Email</em> and <em>Confirm Email</em> entries do not match.');
 define('ENTRY_EMAIL_MISMATCH_ERROR_JS', '* The "Email" and "Confirm Email" entries do not match.');
