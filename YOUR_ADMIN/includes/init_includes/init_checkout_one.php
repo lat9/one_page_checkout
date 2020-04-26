570de3889ef4fed18133b5a82e40d72d79ed479e
lat9 <cindy@vinosdefrutastropicales.com>
2020-04-26 11:53:57 -0400

    Update readme/init for v2.3.1 release

--- YOUR_ADMIN/includes/init_includes/init_checkout_one.php
+++ YOUR_ADMIN/includes/init_includes/init_checkout_one.php
@@ -15,7 +15,7 @@ if (!defined('IS_ADMIN_FLAG')) {
 // 500-599 ... Registered-account settings
 // 1000+ ..... Debug settings
 //
-define('CHECKOUT_ONE_CURRENT_VERSION', '2.3.1-beta4');
+define('CHECKOUT_ONE_CURRENT_VERSION', '2.3.1');
 define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2020-04-26');
 
 if (isset($_SESSION['admin_id'])) {
