e986c6dde1edafc113d024a327cd3dd5fdcfcefd
lat9 <cindy@vinosdefrutastropicales.com>
2021-06-14 12:23:43 -0400

    #296: Correct CSS styling for `checkout_one` page's overlay

--- YOUR_ADMIN/includes/init_includes/init_checkout_one.php
+++ YOUR_ADMIN/includes/init_includes/init_checkout_one.php
@@ -15,8 +15,8 @@ if (!defined('IS_ADMIN_FLAG')) {
 // 500-599 ... Registered-account settings
 // 1000+ ..... Debug settings
 //
-define('CHECKOUT_ONE_CURRENT_VERSION', '2.3.8');
-define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2021-04-17');
+define('CHECKOUT_ONE_CURRENT_VERSION', '2.3.9-beta1');
+define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2021-06-14');
 
 if (isset($_SESSION['admin_id'])) {
     $version_release_date = CHECKOUT_ONE_CURRENT_VERSION . ' (' . CHECKOUT_ONE_CURRENT_UPDATE_DATE . ')';
