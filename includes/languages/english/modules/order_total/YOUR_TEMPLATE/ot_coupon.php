66763596da9dba00cf0f4f9b66a8af31094af91b
lat9 <cindy@vinosdefrutastropicales.com>
2022-02-09 08:55:54 -0500

    #303: Update `ot_coupon` language override to include zc157 definitions.

--- includes/languages/english/modules/order_total/YOUR_TEMPLATE/ot_coupon.php
+++ includes/languages/english/modules/order_total/YOUR_TEMPLATE/ot_coupon.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @package languageDefines
- * @copyright Copyright 2003-2007 Zen Cart Development Team
+ * @copyright Copyright 2003-2020 Zen Cart Development Team
  * @copyright Portions Copyright 2003 osCommerce
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version $Id: ot_coupon.php 6099 2007-04-01 10:22:42Z wilt $
+ * @version $Id: Scott C Wilson 2020 Apr 10 Modified in v1.5.7 $
  */
 
   define('MODULE_ORDER_TOTAL_COUPON_TITLE', 'Discount Coupon');
@@ -24,7 +23,7 @@ if (defined ('CHECKOUT_ONE_ENABLED') && CHECKOUT_ONE_ENABLED == 'true') {
 //-eof-one_page_checkout-lat9  *** 1 of 1 ***
 
   define('MODULE_ORDER_TOTAL_COUPON_TEXT_CURRENT_CODE', 'Your Current Redemption Code: ');
-  define('MODULE_ORDER_TOTAL_COUPON_REMOVE_INSTRUCTIONS', '<p>To remove a Discount Coupon from this order type REMOVE and press Enter or Return</p>');
+  define('TEXT_COMMAND_TO_DELETE_CURRENT_COUPON_FROM_ORDER', 'REMOVE');
+  define('MODULE_ORDER_TOTAL_COUPON_REMOVE_INSTRUCTIONS', '<p>To remove a Discount Coupon from this order replace the coupon code with: ' . TEXT_COMMAND_TO_DELETE_CURRENT_COUPON_FROM_ORDER . '</p>');
   define('TEXT_REMOVE_REDEEM_COUPON', 'Discount Coupon Removed by Request!');
   define('MODULE_ORDER_TOTAL_COUPON_INCLUDE_ERROR', ' Setting Include tax = true, should only happen when recalculate = None');
-?>
