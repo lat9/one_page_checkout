81be19897f2ec765d71f068c18593758f1861390
lat9 <cindy@vinosdefrutastropicales.com>
2022-06-18 19:22:39 -0400

    #339: Remove no longer required language definition overrides for `ot_coupon`.

--- includes/languages/english/modules/order_total/YOUR_TEMPLATE/ot_coupon.php
+++ includes/languages/english/modules/order_total/YOUR_TEMPLATE/ot_coupon.php
@@ -10,12 +10,9 @@
   define('MODULE_ORDER_TOTAL_COUPON_HEADER', TEXT_GV_NAMES . '/Discount Coupon');
   define('MODULE_ORDER_TOTAL_COUPON_DESCRIPTION', 'Discount Coupon');
   define('MODULE_ORDER_TOTAL_COUPON_TEXT_ENTER_CODE', TEXT_GV_REDEEM);
-  define('SHIPPING_NOT_INCLUDED', ' [Shipping not included]');
-  define('TAX_NOT_INCLUDED', ' [Tax not included]');
-  define('IMAGE_REDEEM_VOUCHER', 'Redeem Voucher');
-  
+
 //-bof-one_page_checkout-lat9  *** 1 of 1 ***
-if (defined ('CHECKOUT_ONE_ENABLED') && CHECKOUT_ONE_ENABLED == 'true') {
+if (defined ('CHECKOUT_ONE_ENABLED') && CHECKOUT_ONE_ENABLED === 'true') {
   define('MODULE_ORDER_TOTAL_COUPON_REDEEM_INSTRUCTIONS', '<p>Please type your coupon code into the discount code box below. Your coupon will be applied to the total and reflected in your order\'s display after you click the button to the right or submit your order. Please note: you may only use one coupon per order.</p>');
 } else {
   define('MODULE_ORDER_TOTAL_COUPON_REDEEM_INSTRUCTIONS', '<p>Please type your coupon code into the discount code box below. Your coupon will be applied to the total and reflected in your cart after you click continue. Please note: you may only use one coupon per order.</p>');
