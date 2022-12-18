a74f5d989754c8193ae5a2e2c3f59b8c64f0141a
lat9 <lat9@vinosdefrutastropicales.com>
2022-12-18 17:24:23 -0500

    #367: No 'type="text/javascript"' on script tags

--- includes/modules/pages/create_account/jscript_form_check_register.php
+++ includes/modules/pages/create_account/jscript_form_check_register.php
@@ -1,7 +1,9 @@
 <?php
 // -----
 // Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9 (cindy@vinosdefrutastropicales.com).
-// Copyright (C) 2018, Vinos de Frutas Tropicales.  All rights reserved.
+// Copyright (C) 2018-2022, Vinos de Frutas Tropicales.  All rights reserved.
+//
+// Last updated for OPC v2.4.5.
 //
 /**
  * jscript_form_check
@@ -25,11 +27,11 @@ if (!(isset($_SESSION['opc']) && $_SESSION['opc']->accountRegistrationEnabled())
 // and variables defined in the create_account page's jscript_form_check.php module!
 //
 ?>
-<script type="text/javascript"><!--
-function check_register_form() 
+<script>
+function check_register_form()
 {
     form = 'create_account';
-    
+
     if (submitted == true) {
         alert("<?php echo JS_ERROR_SUBMITTED; ?>");
         return false;
@@ -37,41 +39,38 @@ function check_register_form()
 
     error = false;
     error_message = "<?php echo JS_ERROR; ?>";
-
-<?php 
-if (ACCOUNT_GENDER == 'true') {
+<?php
+if (ACCOUNT_GENDER === 'true') {
 ?>
     check_radio("gender", "<?php echo ENTRY_GENDER_ERROR; ?>");
 <?php
 }
 
-if ((int)ENTRY_FIRST_NAME_MIN_LENGTH > 0) { 
+if ((int)ENTRY_FIRST_NAME_MIN_LENGTH > 0) {
 ?>
     check_input("firstname", <?php echo (int)ENTRY_FIRST_NAME_MIN_LENGTH; ?>, "<?php echo ENTRY_FIRST_NAME_ERROR; ?>");
-<?php 
-} 
+<?php
+}
 
-if ((int)ENTRY_LAST_NAME_MIN_LENGTH > 0) { 
+if ((int)ENTRY_LAST_NAME_MIN_LENGTH > 0) {
 ?>
     check_input("lastname", <?php echo (int)ENTRY_LAST_NAME_MIN_LENGTH; ?>, "<?php echo ENTRY_LAST_NAME_ERROR; ?>");
-<?php 
-} 
-?>
+<?php
+}
 
-<?php 
-if (ACCOUNT_DOB == 'true' && (int)ENTRY_DOB_MIN_LENGTH != 0) {
+if (ACCOUNT_DOB === 'true' && (int)ENTRY_DOB_MIN_LENGTH !== 0) {
 ?>
     check_input("dob", <?php echo (int)ENTRY_DOB_MIN_LENGTH; ?>, "<?php echo ENTRY_DATE_OF_BIRTH_ERROR; ?>");
 <?php
 }
- 
-if (ACCOUNT_COMPANY == 'true' && (int)ENTRY_COMPANY_MIN_LENGTH != 0) {
+
+if (ACCOUNT_COMPANY === 'true' && (int)ENTRY_COMPANY_MIN_LENGTH !== 0) {
 ?>
     check_input("company", <?php echo (int)ENTRY_COMPANY_MIN_LENGTH; ?>, "<?php echo ENTRY_COMPANY_ERROR; ?>");
 <?php
 }
 
-if ((int)ENTRY_EMAIL_ADDRESS_MIN_LENGTH > 0) { 
+if ((int)ENTRY_EMAIL_ADDRESS_MIN_LENGTH > 0) {
 ?>
     check_input("email_address", <?php echo (int)ENTRY_EMAIL_ADDRESS_MIN_LENGTH; ?>, "<?php echo ENTRY_EMAIL_ADDRESS_ERROR; ?>");
     
@@ -79,24 +78,22 @@ if ((int)ENTRY_EMAIL_ADDRESS_MIN_LENGTH > 0) {
         error_message = error_message + '<?php echo ENTRY_EMAIL_MISMATCH_ERROR_JS; ?>' + "\n";
         error = true;
     }
-<?php 
+<?php
 }
 
-if ((int)ENTRY_TELEPHONE_MIN_LENGTH > 0) { 
+if ((int)ENTRY_TELEPHONE_MIN_LENGTH > 0) {
 ?>
     check_input("telephone", <?php echo ENTRY_TELEPHONE_MIN_LENGTH; ?>, "<?php echo ENTRY_TELEPHONE_NUMBER_ERROR; ?>");
-<?php 
-} 
+<?php
+}
 
-
-if ((int)ENTRY_PASSWORD_MIN_LENGTH > 0) { 
+if ((int)ENTRY_PASSWORD_MIN_LENGTH > 0) {
 ?>
     check_password("password", "confirmation", <?php echo (int)ENTRY_PASSWORD_MIN_LENGTH; ?>, "<?php echo ENTRY_PASSWORD_ERROR; ?>", "<?php echo ENTRY_PASSWORD_ERROR_NOT_MATCHING; ?>");
     check_password_new("password_current", "password_new", "password_confirmation", <?php echo (int)ENTRY_PASSWORD_MIN_LENGTH; ?>, "<?php echo ENTRY_PASSWORD_ERROR; ?>", "<?php echo ENTRY_PASSWORD_NEW_ERROR; ?>", "<?php echo ENTRY_PASSWORD_NEW_ERROR_NOT_MATCHING; ?>");
-<?php 
-} 
+<?php
+}
 ?>
-
     if (error == true) {
         alert(error_message);
         return false;
@@ -105,4 +102,4 @@ if ((int)ENTRY_PASSWORD_MIN_LENGTH > 0) {
         return true;
     }
 }
-//--></script>
+</script>
