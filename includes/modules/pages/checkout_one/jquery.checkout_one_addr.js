8bfce33eeb084a550768593a0556ef7112a61f15
lat9 <lat9@vinosdefrutastropicales.com>
2023-08-19 11:04:05 -0400

    #381: Correct state-field visibility, invalid form control

--- includes/modules/pages/checkout_one/jquery.checkout_one_addr.js
+++ includes/modules/pages/checkout_one/jquery.checkout_one_addr.js
@@ -1,8 +1,8 @@
 // -----
 // Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9.
-// Copyright (C) 2018-2022, Vinos de Frutas Tropicales.  All rights reserved.
+// Copyright (C) 2018-2023, Vinos de Frutas Tropicales.  All rights reserved.
 //
-// Last updated: OPC v2.4.4
+// Last updated: OPC v2.4.6
 //
 
 // -----
@@ -10,10 +10,28 @@
 //
 // Note: Starting with OPC v2.3.2, this script is *always* loaded.
 //
+(function($) {
+    $.fn.state = function(action, state_required) {
+        if (action === 'show') {
+            this.show(); 
+            if (state_required && (this.attr('id') === 'state-bill' || this.attr('id') === 'state-ship')) {
+                document.getElementById(this.attr('id')).setAttribute('required', '');
+            }
+        } else {
+            this.hide();
+            if (state_required && (this.attr('id') === 'state-bill' || this.attr('id') === 'state-ship')) {
+                document.getElementById(this.attr('id')).removeAttribute('required');
+            }
+        }
+        return this;
+    };
+}( jQuery ));
+
 jQuery(document).ready(function() {
     var last_country_bill = jQuery('#country-bill option:selected').val();
     var last_country_ship = jQuery('#country-ship option:selected').val();
-    
+    var state_required = jQuery('#state-bill').prop('required');
+
     // -----
     // Initialize the display for the dropdown vs. hand-entry of the state fields.  If the initially-selected
     // country doesn't have zones, the dropdown will contain only 1 element ('Type a choice below ...').
@@ -21,16 +39,16 @@ jQuery(document).ready(function() {
     initializeStateZones = function() 
     {
         if (jQuery('#stateZone-bill > option').length == 1) {
-            jQuery('#stateZone-bill, #stateZone-bill+span, #stateZone-bill+span+br').hide();
+            jQuery('#stateZone-bill, #stateZone-bill+span, #stateZone-bill+span+br').state('hide', state_required);
         } else {
-            jQuery('#state-bill, #state-bill+span').hide();
-            jQuery('#stateZone-bill').show();
+            jQuery('#state-bill, #state-bill+span').state('hide', state_required);
+            jQuery('#stateZone-bill').state('show', state_required);
         }
         if (jQuery('#stateZone-ship > option').length == 1) {
-            jQuery('#stateZone-ship, #stateZone-ship+span, #stateZone-ship+span+br').hide();
+            jQuery('#stateZone-ship, #stateZone-ship+span, #stateZone-ship+span+br').state('hide', state_required);
         } else {
-            jQuery('#state-ship, #state-ship+span').hide();
-            jQuery('#stateZone-ship').show();
+            jQuery('#state-ship, #state-ship+span').state('hide', state_required);
+            jQuery('#stateZone-ship').state('show', state_required);
         }
     }
     initializeStateZones();
@@ -75,12 +93,12 @@ jQuery(document).ready(function() {
             }
         });
         if (countryHasZones) {
-            jQuery('#state-'+which+', #state-'+which+'+span').hide();
+            jQuery('#state-'+which+', #state-'+which+'+span').state('hide', state_required);
             jQuery('#stateZone-'+which).html(countryZones);
-            jQuery('#stateZone-'+which+', #stateZone-'+which+'+span'+', #stateZone-'+which+'+span+br').show();
+            jQuery('#stateZone-'+which+', #stateZone-'+which+'+span'+', #stateZone-'+which+'+span+br').state('show', state_required);
         } else {
-            jQuery('#stateZone-'+which+', #stateZone-'+which+'+span'+', #stateZone-'+which+'+span+br').hide();
-            jQuery('#state-'+which+', #state-'+which+'+span').show();
+            jQuery('#stateZone-'+which+', #stateZone-'+which+'+span'+', #stateZone-'+which+'+span+br').state('hide', state_required);
+            jQuery('#state-'+which+', #state-'+which+'+span').state('show', state_required);
         }
     }
 });
