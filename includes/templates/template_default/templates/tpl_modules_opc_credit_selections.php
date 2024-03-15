e23081d8906a48e039410b36e2cbc66d57872357
lat9 <lat9@vinosdefrutastropicales.com>
2024-03-14 20:23:08 -0400

    #397: Account for credit-selections that don't have selections

--- includes/templates/template_default/templates/tpl_modules_opc_credit_selections.php
+++ includes/templates/template_default/templates/tpl_modules_opc_credit_selections.php
@@ -1,7 +1,9 @@
 <?php
 // -----
 // Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9 (cindy@vinosdefrutastropicales.com).
-// Copyright (C) 2013-2019, Vinos de Frutas Tropicales.  All rights reserved.
+// Copyright (C) 2013-2024, Vinos de Frutas Tropicales.  All rights reserved.
+//
+// Last updated: OPC v2.5.0
 //
 ?>
 <!--bof credit-selection block -->
@@ -23,35 +25,52 @@ if ($shipping_module_available) {
         if (empty($current_selection) || !$_SESSION['opc']->enableCreditSelection($current_selection['id'])) {
             continue;
         }
-        
+
         if (isset($_GET['credit_class_error_code']) && $_GET['credit_class_error_code'] == $current_selection['id']) {
 ?>
-    <div class="messageStackError"><?php echo zen_output_string_protected($_GET['credit_class_error']); ?></div>
+    <div class="messageStackError">
+        <?= zen_output_string_protected($_GET['credit_class_error']) ?>
+    </div>
 
 <?php
         }
+
         // -----
         // Determine which parameter needs to be submitted on the button-formatting to include a common class to which
         // a jQuery event handler binds.  When CSS buttons are used, the "secondary class" input must be used; otherwise,
         // the class is submitted to the function as part of the to-be-created parameter list.
         //
         $ot_class = str_replace('ot_', '', $current_selection['id']);
-        if (strtolower(IMAGE_USE_CSS_BUTTONS) == 'yes') {
+        if (strtolower(IMAGE_USE_CSS_BUTTONS) === 'yes') {
             $secondary_class = 'opc-cc-submit';
             $additional_parms = '';
         } else {
             $secondary_class = '';
             $additional_parms = 'name="apply_' . $ot_class . '"' . ' class="opc-cc-submit"';
         }
-        
+
         foreach ($current_selection['fields'] as $current_field) {
 ?>
-    <div class="checkoutOne<?php echo ucfirst($ot_class); ?>">
+    <div class="checkoutOne<?= ucfirst($ot_class) ?>">
         <fieldset>
-            <legend><?php echo $current_selection['module']; ?></legend><?php echo $current_selection['redeem_instructions']; ?>
-            <div class="gvBal larger"><?php echo (!empty($current_selection['checkbox'])) ? $current_selection['checkbox'] : ''; ?></div>
-            <label class="inputLabel"<?php echo (!empty($current_field['tag'])) ? (' for="' . $current_field['tag'] . '"') : ''; ?>><?php echo $current_field['title']; ?></label><?php echo $current_field['field']; ?>
-            <div class="buttonRow forward"><?php echo zen_image_button(BUTTON_IMAGE_SUBMIT, ALT_TEXT_APPLY_DEDUCTION, $additional_parms, $secondary_class); ?></div>
+            <legend><?= $current_selection['module'] ?></legend>
+            <?= $current_selection['redeem_instructions'] ?>
+            <div class="gvBal larger">
+                <?= (!empty($current_selection['checkbox'])) ? $current_selection['checkbox'] : '' ?>
+            </div>
+            <label class="inputLabel"<?=(!empty($current_field['tag'])) ? (' for="' . $current_field['tag'] . '"') : '' ?>>
+                <?= $current_field['title'] ?>
+            </label>
+            <?= $current_field['field'] ?? '' ?>
+<?php
+            if (!empty($current_selection['checkbox']) || !empty($current_field['field'])) {
+?>
+            <div class="buttonRow forward">
+                <?= zen_image_button(BUTTON_IMAGE_SUBMIT, ALT_TEXT_APPLY_DEDUCTION, $additional_parms, $secondary_class) ?>
+            </div>
+<?php
+            }
+?>
             <div class="clearBoth"></div>
         </fieldset>
     </div>
