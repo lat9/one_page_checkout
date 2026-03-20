bb9e7d98747add13bef99ac7687f89ed84c178e9
lat9 <cindy@vinosdefrutastropicales.com>
2026-03-20 14:23:07 -0400

    Payment choices, refactoring
    - Using short-echo tags
    - Using exactly-equal-to where appropriate
    - Removing `/` from self-closing HTML tags

--- includes/templates/template_default/templates/tpl_modules_opc_payment_choices.php
+++ includes/templates/template_default/templates/tpl_modules_opc_payment_choices.php
@@ -20,34 +20,34 @@ if ($shipping_module_available && $display_payment_block) {
 ?>
   <div id="checkoutPaymentMethod" class="floatingBox forward clearRight">
     <fieldset>
-      <legend><?php echo TABLE_HEADING_PAYMENT_METHOD; ?></legend>
+      <legend><?= TABLE_HEADING_PAYMENT_METHOD ?></legend>
 <?php 
     // ** BEGIN PAYPAL EXPRESS CHECKOUT **
     if (!$payment_modules->in_special_checkout()) {
     // ** END PAYPAL EXPRESS CHECKOUT ** 
-        if (SHOW_ACCEPTED_CREDIT_CARDS != '0') {
-            if (SHOW_ACCEPTED_CREDIT_CARDS == '1') {
+        if (SHOW_ACCEPTED_CREDIT_CARDS !== '0') {
+            if (SHOW_ACCEPTED_CREDIT_CARDS === '1') {
                 echo TEXT_ACCEPTED_CREDIT_CARDS . zen_get_cc_enabled();
-          
-            } elseif (SHOW_ACCEPTED_CREDIT_CARDS == '2') {
+
+            } elseif (SHOW_ACCEPTED_CREDIT_CARDS === '2') {
                 echo TEXT_ACCEPTED_CREDIT_CARDS . zen_get_cc_enabled('IMAGE_');
-          
+
             }
 ?>
-      <br class="clearBoth" />
+      <br class="clearBoth">
 <?php 
-    } 
+        } 
 
         $selection = $enabled_payment_modules;
         $num_selections = count($selection);
 
         if ($num_selections > 1) {
 ?>
-      <p class="important"><?php echo TEXT_SELECT_PAYMENT_METHOD; ?></p>
+      <p class="important"><?= TEXT_SELECT_PAYMENT_METHOD ?></p>
 <?php
-        } elseif ($num_selections == 0) {
+        } elseif ($num_selections === 0) {
 ?>
-      <p class="important"><?php echo TEXT_NO_PAYMENT_OPTIONS_AVAILABLE; ?></p>
+      <p class="important"><?= TEXT_NO_PAYMENT_OPTIONS_AVAILABLE ?></p>
 
 <?php
         }
@@ -55,7 +55,9 @@ if ($shipping_module_available && $display_payment_block) {
         $radio_buttons = 0;
 
         foreach ($selection as $current_method) {
-        echo '<div class="custom-control custom-radio">'; 
+?>
+      <div class="custom-control custom-radio">
+<?php
             $payment_id = $current_method['id'];
             if ($num_selections > 1) {
                 if (empty($current_method['noradio'])) {
@@ -66,7 +68,7 @@ if ($shipping_module_available && $display_payment_block) {
                 echo zen_draw_hidden_field('payment', $payment_id, 'id="pmt-' . $payment_id . '"');
             }
 ?>
-      <label for="pmt-<?php echo $payment_id; ?>" class="custom-control-label radioButtonLabel"><?php echo $current_method['module']; ?></label>
+        <label for="pmt-<?= $payment_id ?>" class="custom-control-label radioButtonLabel"><?= $current_method['module'] ?></label>
       </div>
 <?php
             if (defined('MODULE_ORDER_TOTAL_COD_STATUS') && MODULE_ORDER_TOTAL_COD_STATUS == 'true' && $payment_id == 'cod') {
@@ -82,7 +84,7 @@ if ($shipping_module_available && $display_payment_block) {
                     }
                 }
 ?>
-      <div class="alert"><?php echo TEXT_INFO_COD_FEES; ?></div>
+      <div class="alert"><?= TEXT_INFO_COD_FEES ?></div>
 <?php
             }
 
@@ -92,17 +94,15 @@ if ($shipping_module_available && $display_payment_block) {
 <?php
             }
 ?>
-      <br class="clearBoth" />
-
+      <br class="clearBoth">
 <?php
             if (isset($current_method['error'])) {
 ?>
-      <div><?php echo $current_method['error']; ?></div>
+      <div><?= $current_method['error'] ?></div>
 
 <?php
-            } elseif (isset($current_method['fields']) && is_array($current_method['fields'])) {
+            } elseif (is_array($current_method['fields'] ?? '')) {
 ?>
-
       <div class="ccinfo">
 <?php
                 foreach ($current_method['fields'] as $current_field) {
@@ -112,21 +112,21 @@ if ($shipping_module_available && $display_payment_block) {
                     //
                     if (!empty($current_field['title'])) {
 ?>
-        <label <?php echo (isset($current_field['tag']) ? 'for="' . $current_field['tag'] . '" ' : ''); ?>class="inputLabelPayment"><?php echo $current_field['title']; ?></label>
+        <label <?= (isset($current_field['tag']) ? 'for="' . $current_field['tag'] . '" ' : '') ?>class="inputLabelPayment"><?= $current_field['title'] ?></label>
 <?php
                     }
-                    
+
                     // -----
                     // The 'field' value is **always** required; output it.
                     //
                     echo $current_field['field']; 
 ?>
-        <br class="clearBoth" />
+        <br class="clearBoth">
 <?php
                 }
 ?>
       </div>
-      <br class="clearBoth" />
+      <br class="clearBoth">
 <?php
             }
             $radio_buttons++;
@@ -135,8 +135,8 @@ if ($shipping_module_available && $display_payment_block) {
     // ** BEGIN PAYPAL EXPRESS CHECKOUT **
     } else {
 ?>
-    <p><?php echo ${$_SESSION['payment']}->title; ?></p>
-    <input type="hidden" name="payment" value="<?php echo $_SESSION['payment']; ?>" />
+    <p><?= ${$_SESSION['payment']}->title ?></p>
+    <input type="hidden" name="payment" value="<?= $_SESSION['payment'] ?>">
 <?php
     }
     // ** END PAYPAL EXPRESS CHECKOUT **
