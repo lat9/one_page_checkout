ae2c04f51d10b0a0e3f9fd32cfe82dc6d9cc0fef
lat9 <cindy@vinosdefrutastropicales.com>
2026-03-20 12:18:49 -0400

    `FILENAME_ORDER_STATUS` defined in zc158+
    Ref #398
    
    Don't need to conditionally define it!

--- YOUR_ADMIN/includes/init_includes/init_checkout_one.php
+++ YOUR_ADMIN/includes/init_includes/init_checkout_one.php
@@ -1,7 +1,7 @@
 <?php
 // -----
 // Part of the One-Page Checkout plugin, provided under GPL 2.0 license by lat9
-// Copyright (C) 2013-2025, Vinos de Frutas Tropicales.  All rights reserved.
+// Copyright (C) 2013-2026, Vinos de Frutas Tropicales.  All rights reserved.
 //
 if (!defined('IS_ADMIN_FLAG')) {
     die('Illegal Access');
@@ -16,7 +16,7 @@ if (!defined('IS_ADMIN_FLAG')) {
 // 1000+ ..... Debug settings
 //
 define('CHECKOUT_ONE_CURRENT_VERSION', '2.6.0-beta1');
-define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2025-12-05');
+define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2026-03-20');
 
 if (isset($_SESSION['admin_id'])) {
     $version_release_date = CHECKOUT_ONE_CURRENT_VERSION . ' (' . CHECKOUT_ONE_CURRENT_UPDATE_DATE . ')';
