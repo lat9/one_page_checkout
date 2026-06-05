7040f7a2989b1ecba1727634c8f69e40b86728e6
lat9 <cindy@vinosdefrutastropicales.com>
2026-06-05 08:21:08 -0400

    Update readmes/init for v4.6.2 release

--- YOUR_ADMIN/includes/init_includes/init_checkout_one.php
+++ YOUR_ADMIN/includes/init_includes/init_checkout_one.php
@@ -15,8 +15,8 @@ if (!defined('IS_ADMIN_FLAG')) {
 // 500-599 ... Registered-account settings
 // 1000+ ..... Debug settings
 //
-define('CHECKOUT_ONE_CURRENT_VERSION', '2.6.2-beta2');
-define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2026-06-01');
+define('CHECKOUT_ONE_CURRENT_VERSION', '2.6.2');
+define('CHECKOUT_ONE_CURRENT_UPDATE_DATE', '2026-06-05');
 
 if (isset($_SESSION['admin_id'])) {
     $version_release_date = CHECKOUT_ONE_CURRENT_VERSION . ' (' . CHECKOUT_ONE_CURRENT_UPDATE_DATE . ')';
